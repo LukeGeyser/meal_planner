72121329bd24ad6ee714df9ad1bba53f7cb2994d
Wowza <geyserlg@gmail.com>
2019-09-03 22:40:43 +0200

    Initial Commit

---
+++ MealPlanner/MealPlanner/MealPlanner.Android/obj/Debug/81/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 8edd4012255e37a9ec5d70c0bdeb99b7902e1113 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1176
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 308113
+	.include	"typemap.jm.inc"
