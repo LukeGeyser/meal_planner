6a12344ac7e83972939abf1a2420a073c8841ebd
Wowza <geyserlg@gmail.com>
2019-09-06 16:17:19 +0200

    Changes Directorys (added mobile and Desktop Directories)

---
+++ Mobile/MealPlanner/MealPlanner/MealPlanner/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealPlanner.ViewModels
+{
+    class SignUpViewModel : BaseViewModel
+    {
+        public SignUpViewModel()
+        {
+            Title = "Sign Up";
+        }
+    }
+}
