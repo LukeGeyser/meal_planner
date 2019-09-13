41a3f28ed2f9d5723f2bc663072d70c9d5bb668c
Rashmika Kalan <kalanrashmika16.rk@gmail.com>
2019-09-13 19:52:01 +0200

    Can add new meal plans and allergies to the database

---
+++ Desktop/MealPlannerDesktop/MealPlannerDesktop/frmAddAllergy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MealPlannerDesktop
+{
+    public partial class frmAddAllergy : Form
+    {
+        public frmAddAllergy()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                frmProfile frp = new frmProfile();
+                frp.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAddAllergy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Allergies allergie = new Allergies();
+                allergie.AllergyID = 0;
+                allergie.AllergyName = txtName.Text;
+                allergie.Description = rtbDesc.Text;
+
+                DataHandler.AddAllergy(allergie);
+
+                txtName.Clear();
+                rtbDesc.Clear();
+
+                frmProfile frp = new frmProfile();
+                frp.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
